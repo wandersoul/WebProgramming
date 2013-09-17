05f8cf2835ae0cb3946b3f135ba907fdfc86f261
Marc <every_dog_has_his_day@hotmail.com>
2013-09-16 21:29:01 -0400

    V 1.2
    Most work done

--- trading_card/header.php
+++ trading_card/header.php
@@ -7,12 +7,14 @@
         ?>
         <!--CSS link for project -->
         <link rel="stylesheet" type="text/css" href="css\stylo.css">
+        
         <!--trial for loading a different style sheet based on screen width
-        <link rel="stylesheet" type="text/css" media="screen and (max-width:800px)" href="css\stylo.css">
+        <link rel="stylesheet" type="text/css" media="screen and (max-width:800px)" href="css\stylo.css"
         -->
+        <!--Fancy Font from google-->
+        <link href='http://fonts.googleapis.com/css?family=Playfair+Display+SC:400,900italic' rel='stylesheet' type='text/css'>
         <!--JavaScript sources-->
         <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-        <script src="jquery-ui-1.10.3.custom/js/jquery.quickflip.js"></script>
     </head>
 <body>
 <?php
--- trading_card/index.php
+++ trading_card/index.php
@@ -7,42 +7,47 @@
 <!--Trading Card Section-->
 <section>
     <!--Card Face, on top of border-->
-    <div>
+    <div id="face">
         <!--Header division of card-->
         <div id="head">
             <!--Header for the title of the card-->
             <h1>Marc Anderson</h1>
             <!--Icons for card costing-->
-            <img>
+            <img src="http://webdesign4.georgianc.on.ca/~200235076/AdvWebPro/trading_card/image/White.png" alt="White Mana Symbol" />
+            <img src="http://webdesign4.georgianc.on.ca/~200235076/AdvWebPro/trading_card/image/White.png" alt="White Mana Symbol" />
+            <img src="http://webdesign4.georgianc.on.ca/~200235076/AdvWebPro/trading_card/image/Black.png" alt="Black Mana Symbol"/>
+            <img src="http://webdesign4.georgianc.on.ca/~200235076/AdvWebPro/trading_card/image/Black.png" alt="Black Mana Symbol"/>
         </div>
         
         <!--Image Container-->
         <div id="art">
             <!--Card Art, Picture of Marc-->
-            <img>
+            <img src="http://webdesign4.georgianc.on.ca/~200235076/AdvWebPro/trading_card/image/card_art2.png" alt="Image of Marc Anderson"/>
         </div>
         
         <!--Types Container-->
-        <div id="art">
+        <div id="type">
             <!--headers for card types-->
-            <h2>Legendary Creature -- Human Wizard</h2>
+            <h2>Legendary Creature - Human Wizard</h2>
+            <!--Icon for card edition-->
+            <p id="icon">M</p>
         </div>
         
         <!--Rules Container-->
         <div id="rules">
             <!--Container for rules-->
-            <p></p>
+            <p class="rule">Lifelink</p>
+            <p class="rule">Reach</p>
+            <p class="rule">When Marc Anderson enters the battlefield, sacrifice another creature you control. You gain X life and draw X cards, where X is that creature's power.</p>
             <!--Container for flavor text-->
-            <p class="flavor"><p>
+            <p class="flavor">"Let us tip the scales and see how your 'balance' serves you."<p>
         </div>
         
-        <!--Image Container-->
+        <!--tiny footer indicating power-->
         <div id="foot">
-            <!--Card Art, Picture of Marc-->
+            <!--Creature Power and Toughness-->
+            <p id="power_tough">3/4</p>
         </div>
-        
-        
-        
     </div>
     
 </section>
